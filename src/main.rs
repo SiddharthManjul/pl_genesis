91e67bbe32e8ff9e0ca99b1a4e5823aeee26e524
SiddharthManjul <manjulsiddharth09@gmail.com>
2025-06-16 00:02:04 +0530

    Tokio Executor Changed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use libp2p::{
-    identity, noise, ping, tcp, yamux, Multiaddr, PeerId, Swarm, SwarmBuilder
+    identity, noise, ping, tcp, yamux, Multiaddr, PeerId, Swarm, SwarmBuilder,
+    core::Transport
 };
 use std::error::Error;
 use tokio::io;
@@ -12,5 +13,19 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let local_key = identity::Keypair::generate_ed25519();
     let local_peer_id = PeerId::from(local_key.public());
     println!("Local peer id: {:?}", local_peer_id);
+
+    let transport = tcp::tokio::Transport::new(tcp::Config::default().nodelay(true))
+        .upgrade(libp2p::core::upgrade::Version::V1)
+        .authenticate(noise::Config::new(&local_key)?)
+        .multiplex(yamux::Config::default())
+        .boxed();
+
+    let behaviour = ping::Behaviour::new(ping::Config::new().with_interval(std::time::Duration::from_secs(15)));
+
+    let mut swarm = SwarmBuilder::with_existing_identity(local_key)
+        .with_tokio()
+        .with_tcp(tcp::Config::default(), noise::Config::new, yamux::Config::default)?
+        .with_behaviour(|_| behaviour)?
+        .build();
     Ok(())
 }
